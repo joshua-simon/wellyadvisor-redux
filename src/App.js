85990db29ee84ddc703604192c312d5023fbb398
Joshua Simon <joshy.d.simon@gmail.com>
2022-11-11 12:33:34 +1300

    firebase query working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react'
+import { collection,query,getDocs } from "firebase/firestore";
+import { db } from './firebaseConfig'
+
 
 const App = () => {
-  return (
-    <div>
-      test
+  const [ data,setData] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const itemArray = []
+      const q =  query(collection(db, "test-data"))
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach(doc => itemArray.push(doc.data()))
+      setData(itemArray)
+    }
+    getData()
+  },[])
+
+ const testData = data.map((item,i) => (
+  <div key = {i}>
+      <h2>{item.Title}</h2>
+      <p>{item.Blurb}</p>
     </div>
+ ))
+
+  return (
+   <div>{testData}</div>
   );
 }
 
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,15 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyCmKjjuGNim-PQetLQi-7c8a6cPLqK7Uqw",
   authDomain: "wellyadvisor-with-redux.firebaseapp.com",
   projectId: "wellyadvisor-with-redux",
   storageBucket: "wellyadvisor-with-redux.appspot.com",
   messagingSenderId: "58837443050",
   appId: "1:58837443050:web:7bd97c0efc71cec8f29293"
-};
+})
 
 
-const app = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp)
+
