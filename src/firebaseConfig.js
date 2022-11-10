edbef7c8429a12043734e8f96ee9352fad5443a0
Joshua Simon <joshy.d.simon@gmail.com>
2022-11-11 11:03:47 +1300

    redux and firebase setup

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmKjjuGNim-PQetLQi-7c8a6cPLqK7Uqw",
+  authDomain: "wellyadvisor-with-redux.firebaseapp.com",
+  projectId: "wellyadvisor-with-redux",
+  storageBucket: "wellyadvisor-with-redux.appspot.com",
+  messagingSenderId: "58837443050",
+  appId: "1:58837443050:web:7bd97c0efc71cec8f29293"
+};
+
+
+const app = initializeApp(firebaseConfig);
