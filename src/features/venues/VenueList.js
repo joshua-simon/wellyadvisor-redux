58434b258dad80e561b99a59d631a957af5dcc42
Joshua Simon <joshy.d.simon@gmail.com>
2022-12-08 13:32:00 +1300

    filter venues feature added

--- src/features/venues/VenueList.js
+++ src/features/venues/VenueList.js
@@ -1,32 +1,105 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 const VenueList = () => {
+  const [venueType, setVenueType] = useState("");
 
   const venues = useSelector((state) => state.venues);
-  const loading = useSelector((state) => state.isLoading)
+  const loading = useSelector((state) => state.isLoading);
 
-  const loadingSection  = <div>loading...</div>
+  const loadingSection = <div>loading...</div>;
 
-  const content = venues.venues.map((venue) => (
-    <div className="venue-item">
-        <Link to={`/venue/${venue.id}`} style = {{textDecoration: "none"}} key = {venue.name}>
-          <img src={venue.photo} />
-        </Link>
-        <p>{venue.name}</p>
-      </div>
-  ));
+  let content;
+  let title;
+  let seeAllVenuesButton;
+
+  if (!venueType) {
+    title = "All venues";
+    seeAllVenuesButton = false;
+    content = venues.venues.map((venue) => {
+      return (
+        <div className="venue-item">
+          <Link
+            to={`/venue/${venue.id}`}
+            style={{ textDecoration: "none" }}
+            key={venue.name}
+          >
+            <img src={venue.photo} />
+          </Link>
+          <p>
+            {venue.name} | {venue.type}
+          </p>
+        </div>
+      );
+    });
+  } else {
+    let filteredVenues = venues.venues.filter(
+      (venue) => venue.type === venueType.venueType
+    );
+    let venuePlural =
+      venueType.venueType === "gallery"
+        ? "galleries"
+        : `${venueType.venueType}s`;
+    seeAllVenuesButton = true;
+    title = `All ${venuePlural}`;
+    content = filteredVenues.map((venue) => {
+      return (
+        <div className="venue-item">
+          <Link
+            to={`/venue/${venue.id}`}
+            style={{ textDecoration: "none" }}
+            key={venue.name}
+          >
+            <img src={venue.photo} />
+          </Link>
+          <p>
+            {venue.name} | {venue.type}
+          </p>
+        </div>
+      );
+    });
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setVenueType((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const selectVenue = (
+    <div className="select-venue">
+      <label>
+        Filter venue by type{" "}
+        <select name="venueType" onChange={handleChange}>
+          <option value="cafe">Cafes</option>
+          <option value="bar">Bars</option>
+          <option value="theatre">Theatres</option>
+          <option value="gallery">Galleries</option>
+        </select>
+      </label>
+    </div>
+  );
 
 
   return (
     <div>
-    <Header/>
+      <Header />
+      {selectVenue}
       <div className="venue-list">
-        {loading ? loadingSection : content}
+        <h2>
+          {title}{" "}
+          {seeAllVenuesButton ? (
+            <button onClick={() => setVenueType("")}>See all venues</button>
+          ) : null}
+        </h2>
+        {content}
       </div>
-    <Footer/>
+      <Footer />
     </div>
   );
 };
